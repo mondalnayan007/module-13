dbe6ae29d60f1d1590cb86ac19b1907f65a5373b
Nayan <n007kingkong@gmail.com>
2025-07-26 22:46:14 +0600

    First problem is solved

---
+++ prob-1.js
@@ -0,0 +1,15 @@
+// You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+
+var myMoney = 1000;
+
+var orangePrice = 300;
+
+var applePrice = 400;
+
+var totalPrice = orangePrice + applePrice;
+
+
+var remainingMoney = myMoney - totalPrice;
+
+console.log(remainingMoney);
